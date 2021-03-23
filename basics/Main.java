fb5136cf8c4d12bb5f4656829f00bd292472ff44
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-03-23 12:49:23 -0400

    Update

--- basics/Main.java
+++ basics/Main.java
@@ -7,7 +7,7 @@ public class Main{
         // System.out.println(pluralize("dog",3));
         // System.out.println(pluralize("dog",1));
         clock();        
-        flipNHeads(6);
+        // flipNHeads(6);
     }
 
     public static String pluralize(String thing, int amount){        
@@ -17,7 +17,7 @@ public class Main{
 
     public static void clock(){
        
-
+        int oldSecond = 9999;
         while(true)
         {
             LocalDateTime time = LocalDateTime.now();
@@ -27,13 +27,16 @@ public class Main{
             String minuteS = minute < 10 ? "0"+minute : ""+minute;
             int seconds = time.getSecond();
             String secondsS = seconds < 10 ? "0"+seconds : ""+seconds;
-
-            System.out.println(hourS + ":" + minuteS + ":" + secondsS);
-            try
+            if(seconds != oldSecond)
             {
-                TimeUnit.SECONDS.sleep(1);
+                System.out.println(hourS + ":" + minuteS + ":" + secondsS);
+                oldSecond = seconds;
             }
-            catch(Exception e){}
+            // try
+            // {
+            //     TimeUnit.SECONDS.sleep(1);
+            // }
+            // catch(Exception e){}
         }       
     }
 
@@ -46,7 +49,7 @@ public class Main{
             double chance = rnd.nextDouble();
             String call = chance < .5 ? "tails" : "heads";
             System.out.println(call);
-            headCounter += call == "heads" ? 1 : 0;
+            headCounter = call == "heads" ? headCounter += 1 : 0;
             if(headCounter == n){break;}
             counter++;
         }
